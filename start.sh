c60d54033a08e1c56a34f4374e51b93d7f80708e
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-06 21:02:02 +0100

    Fix start script

--- start.sh
+++ start.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-docker run -d -v src :/srv -v $(pwd)/Caddyfile:/etc/Caddyfile -v $HOME/.caddy:/root/.caddy  -p 80:80 -p 443:443 abiosoft/caddy
+docker run -d -v $(pwd)/src/:/srv -v $(pwd)/Caddyfile:/etc/Caddyfile -v $HOME/.caddy:/root/.caddy -p 80:80 -p 443:443 abiosoft/caddy
