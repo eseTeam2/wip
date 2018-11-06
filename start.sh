c711cd941a0398b7b24b8208866d59a836753acd
Yannik Daellenbach <yannik.daellenbach@protonmail.com>
2018-11-06 20:58:40 +0100

    Fix Shebang

--- start.sh
+++ start.sh
@@ -1,8 +1,2 @@
-#!bin/bash
-
-docker run -d \
-    -v src :/srv \
-    -v $(pwd)/Caddyfile:/etc/Caddyfile \
-    -v $HOME/.caddy:/root/.caddy \
-    -p 80:80 -p 443:443 \
-    abiosoft/caddy
+#!/bin/sh
+docker run -d -v src :/srv -v $(pwd)/Caddyfile:/etc/Caddyfile -v $HOME/.caddy:/root/.caddy  -p 80:80 -p 443:443 abiosoft/caddy
